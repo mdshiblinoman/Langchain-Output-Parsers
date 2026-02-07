673c5ee214413302887f97432aad2c1fea0f3358
mdshiblinoman <mdshiblisharkar@gmail.com>
2026-02-07 17:10:30 +0600

    update all file in free api key

--- stroutputparser1.py
+++ stroutputparser1.py
@@ -1,4 +1,4 @@
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import GoogleGenerativeAI
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
@@ -6,7 +6,7 @@ from langchain_core.output_parsers import StrOutputParser
 load_dotenv()
 
 
-model = ChatOpenAI()
+model = GoogleGenerativeAI()
 
 # 1st prompt -> detailed report
 template1 = PromptTemplate(
